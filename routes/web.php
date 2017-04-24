bbafe17ca700db8d6c45b7a7343cf0258acca6b6
Semler Romain <romain.semler@gmail.com>
2017-04-24 20:59:27 +0200

    dernier

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+});Route::get('{n}', ['as' => "home", function() { 	return 'Je suis à la page d\'accueil.'; }]);Route::get('{n}', ['as' => "home", function($n) { 	return response('Je suis la page ' . $n . ' !', 200);}])->where('n', '[1-3]');
